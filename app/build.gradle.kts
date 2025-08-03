864428e4dc84accc99fe9c06e34993963b3aa122
sadtoru <daiarocio@outlook.com>
2025-08-03 20:25:58 -0300

    Add Hilt for dependency injection

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,8 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.google.gms.google.services)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt)
 }
 
 android {
@@ -42,25 +44,44 @@ android {
 
 dependencies {
 
+    // Core Android & Lifecycle
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.appcompat)
+
+    // Jetpack Compose
+    implementation(platform(libs.androidx.compose.bom)) // BOM para Compose
     implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-    implementation(libs.firebase.database)
+    implementation(libs.androidx.ui.tooling.preview)
+    // Navegación
+    implementation(libs.androidx.navigation.compose)
+
+    //Hilt + dagger
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.compiler)
+
+    // Firebase
     implementation(libs.firebase.auth)
+    implementation(libs.firebase.database)
+
+    // Google Sign-In & Credentials
     implementation(libs.androidx.credentials)
     implementation(libs.androidx.credentials.play.services.auth)
     implementation(libs.googleid)
-    implementation(libs.androidx.appcompat)
+
+    // Testing
     testImplementation(libs.junit)
+
+    // Android Testing
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(platform(libs.androidx.compose.bom)) // BOM para Compose Tests
     androidTestImplementation(libs.androidx.ui.test.junit4)
+
+    // Debugging - Compose
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 }
