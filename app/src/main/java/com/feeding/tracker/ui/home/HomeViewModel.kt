2a586def88e6dd843cc683c5c46697725f4e8dec
Gothman240 <paez.federico96@gmail.com>
2025-08-05 16:08:09 -0300

    mostrando datos del usuario en home screen

--- app/src/main/java/com/feeding/tracker/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/feeding/tracker/ui/home/HomeViewModel.kt
@@ -1,6 +1,42 @@
 package com.feeding.tracker.ui.home
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.feeding.tracker.domain.useCase.AuthUserUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+data class UserState(
+    val name: String = "",
+    val email: String = "",
+)
 
 @HiltViewModel
 class HomeViewModel
+    @Inject
+    constructor(
+        private val currentUser: AuthUserUseCase,
+    ) : ViewModel() {
+        private var _userUiState = MutableStateFlow<UserState?>(UserState())
+        val userUiState = _userUiState
+
+        init {
+            viewModelScope.launch {
+                currentUser().collect { user ->
+                    if (user != null) {
+                        // User is logged in
+                        userUiState.update {
+                            it?.copy(
+                                email = user.email,
+                            )
+                        }
+                    } else {
+                        // User is not logged in
+                    }
+                }
+            }
+        }
+    }
