cd0aafa4534cc8d0dbdbc7c5b2128fb8eca8d095
Gothman240 <paez.federico96@gmail.com>
2025-08-05 16:04:51 -0300

    Refactor AuthRepository

--- app/src/main/java/com/feeding/tracker/data/repository/AuthRepositoryImpl.kt
+++ app/src/main/java/com/feeding/tracker/data/repository/AuthRepositoryImpl.kt
@@ -1,28 +1,34 @@
 package com.feeding.tracker.data.repository
 
 import com.feeding.tracker.data.datasource.AuthFirebaseDataSource
+import com.feeding.tracker.data.mappers.toDomain
 import com.feeding.tracker.domain.model.UserDomain
 import com.feeding.tracker.domain.repository.AuthRepository
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
 
-class AuthRepositoryImpl(
-    private val authDataSource: AuthFirebaseDataSource,
-) : AuthRepository {
-    override suspend fun login(
-        email: String,
-        password: String,
-    ): Flow<Result<UserDomain>> = authDataSource.login(email, password)
+class AuthRepositoryImpl
+    @Inject
+    constructor(
+        private val authDataSource: AuthFirebaseDataSource,
+    ) : AuthRepository {
+        override suspend fun login(
+            email: String,
+            password: String,
+        ): Flow<Result<UserDomain>> = authDataSource.login(email, password)
 
-    override suspend fun signUp(
-        email: String,
-        password: String,
-    ): Flow<Result<UserDomain>> = authDataSource.signUp(email, password)
+        override suspend fun signUp(
+            email: String,
+            password: String,
+        ): Flow<Result<UserDomain>> = authDataSource.signUp(email, password)
 
-    override fun logout() {
-        TODO("Not yet implemented")
+        override fun logout() {
+            TODO("Not yet implemented")
+        }
+
+        override fun getCurrentUser(): Flow<UserDomain?> =
+            authDataSource.currentUser.map { firebaseUser ->
+                firebaseUser?.toDomain()
+            }
     }
-
-    override fun getCurrentUserId(): String? {
-        TODO("Not yet implemented")
-    }
-}
